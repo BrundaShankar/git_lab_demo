801e719850d9ab59448c457c135f65aed6daec02
Brunda <131005645+BrundaShankar@users.noreply.github.com>
2026-01-30 10:24:01 +0530

    Remove input prompts from Merge class
    Removed input handling code from main method.

--- Merge.java
+++ Merge.java
@@ -3,8 +3,5 @@ public class Merge{
   publi static void main(String[]args){
     Scanner.sc=new Scanner(System.in);
     System.out.println("Enter no of inputs");
-    in= sc.nextInt();
-    System.out.println("Enter inputs:");
-    input=sc.nextInt();
   }
 }
