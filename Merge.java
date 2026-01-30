ba13f9ccb157937920e911bd0db00f34703df369
Brunda <131005645+BrundaShankar@users.noreply.github.com>
2026-01-30 10:34:43 +0530

    Update main method to print Jenkins build trigger message

--- Merge.java
+++ Merge.java
@@ -1,7 +1,5 @@
-import java.util.Scanner;
 public class Merge{
   publi static void main(String[]args){
-    Scanner.sc=new Scanner(System.in);
-    System.out.println("Enter no of inputs");
+    System.out.println("Build Trigger for Jenkins");
   }
 }
